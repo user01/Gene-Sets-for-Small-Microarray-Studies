0cb55ff7e4dfafd003b76bf0e7da52ca8fc1265e
Erik <erik.langenborg@gmail.com>
2017-01-27 16:15:44 -0500

    Added basic PCA reduction code

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 
+results/pca_matrix_1.2.tsv: results/expression_matrix.tsv
+	Rscript dimreduction_pca.R
+
 results/expression_matrix.tsv:
 	Rscript load_data.R
 
---
+++ dimreduction_pca.R
@@ -0,0 +1,18 @@
+suppressPackageStartupMessages({
+  library(readr)
+  library(dplyr)
+})
+
+expression_data <- file.path("results", "expression_matrix.tsv") %>% read_tsv(col_names = FALSE) %>%
+  as.matrix
+
+pca_results <- expression_data %>% t %>% scale(center = TRUE, scale = FALSE) %>%
+  t %>% scale(center = FALSE, scale = TRUE) %>% t %>% prcomp(center = FALSE)
+
+# pca_results %>% glimpse
+
+# plot(pca_results$x[,1:2])
+
+pca_results$x[, 1:2] %>%
+  as.data.frame %>%
+  write_tsv(file.path("results", "pca_matrix_1.2.tsv"))
