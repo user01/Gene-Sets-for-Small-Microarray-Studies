fd8c71fce2b27fa0cdedc6a8ca167ebade3d1003
Erik <erik.langenborg@gmail.com>
2017-01-27 15:54:11 -0500

    filter out invalid record in tsv

--- load_data.R
+++ load_data.R
@@ -11,7 +11,8 @@ EMF_GSVA_Gene_Set_Enrichment <- read_tsv(EMF_GSVA_Gene_Set_Enrichment_PATH, col_
 EMF_GSVA_Relative_Normalilzed_Expression_PATH <- file.path("data", "EMF_GSVA_Relative_Normalilzed_Expression.tsv.tar.gz")
 EMF_GSVA_Relative_Normalilzed_Expression <- read_tsv(EMF_GSVA_Relative_Normalilzed_Expression_PATH,
   col_types = cols(.default = col_double(), EMF_GSVA_Relative_Normalilzed_Expression.tsv = col_character(),
-    Symbol = col_character(), Entrez = col_integer(), Name = col_character()))
+    Symbol = col_character(), Entrez = col_integer(), Name = col_character())) %>%
+    filter(!is.na(Symbol))
 
 Gautier_Immgen_Sample_Metadata_PATH <- file.path("data", "Gautier_Immgen_Sample_Metadata.tsv")
 Gautier_Immgen_Sample_Metadata <- read_tsv(Gautier_Immgen_Sample_Metadata_PATH, col_types = cols(GSM_ID = col_character(),
@@ -21,9 +22,3 @@ Gautier_Immgen_Standardized_Norm_Data_PATH <- file.path("data", "Gautier_Immgen_
 Gautier_Immgen_Standardized_Norm_Data <- read_tsv(Gautier_Immgen_Standardized_Norm_Data_PATH,
   col_types = cols(.default = col_double(), Gautier_Immgen_Standardized_Norm_Data.tsv = col_character(),
     Symbol = col_character(), Entrez = col_integer(), Name = col_character()))
-
-EMF_GSVA_Gene_Set_Enrichment
-EMF_GSVA_Relative_Normalilzed_Expression
-# EMF_GSVA_Relative_Normalilzed_Expression$GSM854326 %>% sd(na.rm=T)
-Gautier_Immgen_Sample_Metadata
-Gautier_Immgen_Standardized_Norm_Data
