72deed455903725963895cf6a787ed2ba82e1807
Erik <erik.langenborg@gmail.com>
2017-01-27 17:23:50 -0500

    PCA now deals with updated raw data

--- dimreduction_pca.R
+++ dimreduction_pca.R
@@ -3,15 +3,27 @@ suppressPackageStartupMessages({
   library(dplyr)
 })
 
-expression_data <- file.path("results", "expression_matrix.tsv") %>% read_tsv(col_names = FALSE) %>%
-  as.matrix
+gene_data_vs_cell_type <- file.path("results", "gene_data_vs_cell_type.tsv") %>% read_tsv(col_types=cols(
+  .default = col_double(),
+  GSM_ID = col_character(),
+  Cell_Type = col_character(),
+  General_Cell_Type = col_character()
+))
 
-pca_results <- expression_data %>% t %>% scale(center = TRUE, scale = FALSE) %>%
-  t %>% scale(center = FALSE, scale = TRUE) %>% t %>% prcomp(center = FALSE)
+expression_data <- gene_data_vs_cell_type %>%
+                   select(-GSM_ID,-Cell_Type,-General_Cell_Type) %>% as.matrix
+
+pca_results <- expression_data %>%
+               scale(center = TRUE, scale = FALSE) %>%
+               t %>%
+               scale(center = FALSE, scale = TRUE) %>%
+               t %>%
+               prcomp(center = FALSE)
 
 # pca_results %>% glimpse
 # plot(pca_results$x[,1:2])
 
 pca_results$x[, 1:2] %>%
   as.data.frame %>%
+  cbind(gene_data_vs_cell_type %>% select(Cell_Type,General_Cell_Type)) %>%
   write_tsv(file.path("results", "dimreduced_matrix_pca_1.2.tsv"))
