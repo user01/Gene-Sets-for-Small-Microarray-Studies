60be6a747a729c3f6b18d706a271a0b3e21aab79
Erik <erik.langenborg@gmail.com>
2017-01-28 14:38:25 -0500

    Cluster basic operation. Hard coded cluster counts

--- cluster_em.R
+++ cluster_em.R
@@ -2,6 +2,7 @@ suppressPackageStartupMessages({
   library(EMCluster)
   library(readr)
   library(dplyr)
+  library(purrr)
   library(stringr)
 })
 
@@ -14,38 +15,75 @@ target_file <- if (str_detect(args[1], "\\.tsv$")) {
   file.path("results", "dimreduced_matrix_pca_1.2.tsv")
 }
 
+data_target <- read_tsv(target_file, col_types = cols(
+  .default = col_double(),
+  Cell_Type = col_character(),
+  General_Cell_Type = col_character()
+))
+# data_target %>% glimpse
 
-target_data <- read_tsv(target_file, col_types = cols(PC1 = col_double(), PC2 = col_double()))
-target_data %>% glimpse
+
+data_pca <- data_target %>% select(-Cell_Type, -General_Cell_Type)
+data_labels <- data_target %>% select(Cell_Type,General_Cell_Type)
+# data_pca %>% glimpse
+# data_labels %>% glimpse
+
+get_top <- function(v) {
+  v %>%
+    table %>%
+    as.data.frame %>%
+    get(".",.) %>%
+    nth(1)
+}
+
+data_range <- 1:nrow(data_target)
+
+data_range %>%
+  map(function(idx){
+
+    # Create data sets
+    data_pca_current <- data_pca %>% filter(idx != data_range)
+    data_labels_current <- data_labels %>% filter(idx != data_range)
+    data_pca_target <- data_pca %>% filter(idx == data_range)
+    data_labels_target <- data_labels %>% filter(idx == data_range)
+
+    # Perform cluster
+    emobj <- emgroup(data_pca_current, nclass = 8)
+    ret <- emcluster(data_pca_current, emobj, assign.class = TRUE)
+
+    data_pca_target %>%
+      assign.class(emobj) %>%
+      get("class",.) -> pca_predicted_class
+
+    data_labels_current %>%
+      mutate(class = emobj %>% get("class",.)) %>%
+      filter(class == pca_predicted_class) -> predicted_class_training
+
+    predicted_class_cell_type <- predicted_class_training %>%
+      get("Cell_Type",.) %>%
+      get_top
+    predicted_class_general_cell_type <- predicted_class_training %>%
+      get("General_Cell_Type",.) %>%
+      get_top
+
+    # # Render cluster plot
+    # png(filename=paste0("results/plot",idx,".png"))
+    # plotem(ret, data_pca_current, main=paste0("Cluster ", idx, " Results"))
+    # points(data_pca_target, pch=23, col="red", bg="red")
+    # dev.off()
+
+    data.frame(
+      idx = idx,
+      Cell_Type = data_labels_target$Cell_Type,
+      Cell_Type_Predicted = predicted_class_cell_type,
+      General_Cell_Type = data_labels_target$General_Cell_Type,
+      General_Cell_Type_Predicted = predicted_class_general_cell_type
+    )
+  }) %>%
+  reduce(rbind) %>%
+  as.data.frame ->
+  data_results
 
 
-
-
-
-
-set.seed(1234)
-
-da2$da
-x2 <- da2$da
-emobj <- emgroup(x2, nclass = 5)
-ret_2 <- emcluster(x2, emobj, assign.class = TRUE)
-
-emobj %>% glimpse
-
-plotem(ret_2, x2)
-
-data.frame(x=5,y=14) %>%
-  assign.class(emobj) %>%
-  glimpse
-
-ret_2 %>% glimpse
-ret_2$class %>% glimpse
-ret_2$class %>% unique %>% glimpse
-
-cbind(x2,ret_2$class) %>% glimpse
-
-ret <- init.EM(x2, nclass = 2)
-ret.new <- assign.class(x2, ret, return.all = FALSE)
-str(ret.new)
-
-emcluster(da2)
+data_results %>%
+  write_tsv(file.path("results", "cluster_pca_results.tsv"))
