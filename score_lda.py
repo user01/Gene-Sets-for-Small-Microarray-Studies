5b14a54b8221e948f678e58dcfa1faa762ab7977
Erik <erik.langenborg@gmail.com>
2017-03-19 00:55:57 -0400

    Added lda scaling detection

--- score_lda.py
+++ score_lda.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import numpy as np
 import pandas as pd
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -32,12 +33,52 @@ args = parser.parse_args()
 # ]))
 
 
-args.input
-
-data_raw = pd.read_table(args.input)
-data = data_raw.assign(truth=data_raw.Cell_Type if args.type ==
-                       'Cell_Type' else data_raw.General_Cell_Type) \
+data_00 = pd.read_table(args.input)
+data_01 = data_00.assign(truth=data_00.Cell_Type if args.type ==
+                         'Cell_Type' else data_00.General_Cell_Type) \
     .drop(['GSM_ID', 'Cell_Type', 'General_Cell_Type'], axis=1)
+
+others = (data_01.truth == args.name).apply(
+    lambda x: args.name if x else "other")
+data_02 = data.assign(truth=others)
+
+data_normal = data_02.query('truth != "other"').reset_index(drop=True)
+data_other = data_02.query('truth == "other"')\
+    .sample(n=data_normal.shape[0], random_state=args.seed)\
+    .reset_index(drop=True)
+
+data_03 = pd.DataFrame.append(data_normal, data_other)\
+    .sample(frac=1, random_state=args.seed + 1)\
+    .reset_index(drop=True)
+
+
+def lda_model(data):
+    X_train = ignore_one(
+        data.drop(['truth'], axis=1), idx).as_matrix()
+    y_train = ignore_one(data[['truth']], idx).as_matrix().T[0]
+
+    clf = LinearDiscriminantAnalysis()
+    clf.fit(X_train, y_train)
+    return clf
+
+
+lda_model_main = lda_model(data_03)
+
+lda_model_main.scalings_.shape
+lda_model_main.scalings_[:, 0].shape
+
+data_03.columns[:-1]
+data_03.columns[:-1].shape
+
+top_genes = math.floor(1 / 2 * (math.sqrt(8 * args.pairs + 1) + 1))
+
+top_gene_names = pd.DataFrame({
+    'gene': data_03.columns[:-1],
+    'loading': np.abs(lda_model_main.scalings_[:, 0])})\
+    .sort_values('loading', ascending=False)\
+    .reset_index(drop=True)\
+    .iloc[:top_genes]
+
 
 print(args.seed)
 print(data.shape)
