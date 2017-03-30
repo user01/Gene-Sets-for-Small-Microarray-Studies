76510765aa2930a924a9ae5be8b072751a3f4c35
Erik Langenborg <erik.langenborg@gmail.com>
2017-03-30 00:49:58 -0400

    Simplified task list

--- make_pairwise.js
+++ make_pairwise.js
@@ -2,87 +2,58 @@ const {
   run_lda_bootstrap,
   read_pairwise_union
 } = require('./utilities/bootstrap.lda.js');
+const R = require('ramda');
 
-const runBase100_200 = () =>
-  run_lda_bootstrap('100_200_base',   // Title
+const runBaseline = () =>
+  run_lda_bootstrap('baseline',   // Title
                     1,        // Bootstraps
                     1.0,      // Fraction to sample
                     4,        // concurrency
                     'python3', // python_binary
                     'fmeasure', // scoring mechanic
-                    100,        // smallest gene set
+                    25,        // smallest gene set
                     200,        // largest gene set size
-                    300,        // number of sets to create
+                    50,        // number of sets to create
+                    500,       // number of gene pairs
                     false);
 
-const runBase200_300 = () =>
-  run_lda_bootstrap('200_300_base',   // Title
-                    1,        // Bootstraps
-                    1.0,      // Fraction to sample
+
+const runTask = (options={}) => {
+  const opts = R.merge({
+    bootstraps: 100,
+    fraction: 0.66,
+    scoring: 'fmeasure',
+    low: 25,
+    high: 200,
+    count: 50,
+    pairs: 500
+  }, options);
+  const title_str = R.pipe(
+    R.values,
+    R.join('.')
+  )(opts);
+  return () => run_lda_bootstrap(`main_${title_str}`,   // Title
+                    opts.bootstraps,        // Bootstraps
+                    opts.fraction,      // Fraction to sample
                     4,        // concurrency
                     'python3', // python_binary
-                    'fmeasure', // scoring mechanic
-                    200,        // smallest gene set
-                    300,        // largest gene set size
-                    300,        // number of sets to create
+                    opts.scoring, // scoring mechanic
+                    opts.low,        // smallest gene set
+                    opts.high,        // largest gene set size
+                    opts.count,        // number of sets to create
+                    opts.pairs,       // number of gene pairs
                     false);
+};
 
 
-const runBase300_400 = () =>
-  run_lda_bootstrap('300_400_base',   // Title
-                    1,        // Bootstraps
-                    1.0,      // Fraction to sample
-                    4,        // concurrency
-                    'python3', // python_binary
-                    'fmeasure', // scoring mechanic
-                    300,        // smallest gene set
-                    400,        // largest gene set size
-                    300,        // number of sets to create
-                    false);
 
-const runMain100_200 = () =>
-  run_lda_bootstrap('100_200_main',   // Title
-                    100,        // Bootstraps
-                    0.66,      // Fraction to sample
-                    4,        // concurrency
-                    'python3', // python_binary
-                    'fmeasure', // scoring mechanic
-                    100,        // smallest gene set
-                    200,        // largest gene set size
-                    300,        // number of sets to create
-                    false);
-
-const runMain200_300 = () =>
-  run_lda_bootstrap('200_300_main',   // Title
-                    100,        // Bootstraps
-                    0.66,      // Fraction to sample
-                    4,        // concurrency
-                    'python3', // python_binary
-                    'fmeasure', // scoring mechanic
-                    200,        // smallest gene set
-                    300,        // largest gene set size
-                    300,        // number of sets to create
-                    false);
-
-
-const runMain300_400 = () =>
-  run_lda_bootstrap('300_400_main',   // Title
-                    100,        // Bootstraps
-                    0.66,      // Fraction to sample
-                    4,        // concurrency
-                    'python3', // python_binary
-                    'fmeasure', // scoring mechanic
-                    300,        // smallest gene set
-                    400,        // largest gene set size
-                    300,        // number of sets to create
-                    false);
-
-
-// runBase100_200()
-  // .then(runBase200_300)
-  // .then(runBase300_400)
-  // .then(runMain200_300)
-
-runMain200_300()
-  .then(runMain300_400)
-  .then(runMain100_200)
+Promise.resolve()
+        .then(runBaseline)
+        .then(runTask())
+        .then(runTask({low:100,high:200}))
+        .then(runTask({low:200,high:300}))
+        .then(runTask({low:300,high:400}))
+        .then(runTask({low:500,high:600}))
+        .then(runTask({bootstraps:200,fraction:0.5}))
+        .then(runTask({bootstraps:200,fraction:0.5,low:200,high:300}))
+        .then(read_pairwise_union)
